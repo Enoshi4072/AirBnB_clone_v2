0060e769841717b36986e50f37eb9439fa26242d
Enoshi4072 <enockkibet6642@gmail.com>
2023-10-08 08:11:13 +0000

    Prepate web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,41 +1,44 @@
 #!/usr/bin/env bash
-# preparing a web server for the deployment of web_static.
-if ! dpkg -l | grep -q nginx; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
-fi
+# Sets up a web server for deployment of web_static.
 
-# Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
+apt-get update
+apt-get install -y nginx
 
-# Create a fake HTML file
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Create or recreate symbolic link
-sudo rm -f /data/web_static/current
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-# Give ownership to the ubuntu user and group recursively
-sudo chown -R ubuntu:ubuntu /data/
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-# Update Nginx configuration
-config_file="/etc/nginx/sites-available/default"
-config_content="location /hbnb_static {
-    alias /data/web_static/current/;
-    index index.html;
-}"
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
 
-# Check if the configuration content is already in the file
-if ! grep -q "$config_content" "$config_file"; then
-    sudo sed -i "/location \/ {/a $config_content" "$config_file"
-fi
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
 
-# Restart Nginx
-sudo service nginx restart
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+service nginx restart
